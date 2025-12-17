846a6d263a5d2313d4640e5c2781ce373b30225e
Michael Ojogbo <linkupxc@gmail.com>
2025-12-17 16:09:36 +0100

    bioup

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
               xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl '/>
               <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>
                As a skilled Software Engineer and Designer, I am dedicated to turning ideas into innovative applications. 
-               Explore my latest projects and articles, showcasing my expertise in Next.js, Expo, Web3 and AI.
+               Explore my latest projects and articles, showcasing my expertise Programming, Design and AI.
               </p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link href="https://read.cv/enwerim" target={"_blank"}
